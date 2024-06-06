860d3978f67b5085ecb4591bc576c5a070dbdf0d
wposso <willintonpiedrahita016@gmail.com>
2024-06-06 17:51:19 -0500

    Modulo 7 Curso LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using LINQ;
+
+var personas = new List<Persona>() {
+    new Persona { Nombre = "Eduardo", Edad = 30, FechaIngresoALaEmpresa = new DateTime(2021, 1, 2), Soltero = true },
+    new Persona { Nombre = "Nidia", Edad = 19, FechaIngresoALaEmpresa = new DateTime(2015, 11, 22), Soltero = true },
+    new Persona { Nombre = "Alejandro", Edad = 45, FechaIngresoALaEmpresa = new DateTime(2020, 4, 12), Soltero = false },
+    new Persona { Nombre = "Valentina", Edad = 24, FechaIngresoALaEmpresa = new DateTime(2021, 7, 8), Soltero = false },
+    new Persona { Nombre = "Roberto", Edad = 61, FechaIngresoALaEmpresa = DateTime.Now.AddDays(-1), Soltero = false },
+};
+
+//Sintaxis metodo
+var personas25AñosOMenos = personas.Where(p => p.Edad <= 25).ToList();
+Console.WriteLine("Validando personas menores de 25 años: ");
+
+foreach (var persona in personas25AñosOMenos) 
+{
+    Console.WriteLine($"{persona.Nombre} es menor de 25 años");
+}
