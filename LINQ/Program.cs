c07749174d9c4050e4e2ee701054bb413c075de1
wposso <willintonpiedrahita016@gmail.com>
2024-06-07 11:49:10 -0500

    Curso LINQ Seccion 3 modulo 14

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,18 +1,9 @@
 ﻿using LINQ;
 
-var personas = new List<Persona>() {
-    new Persona { Nombre = "Eduardo", Edad = 30, FechaIngresoALaEmpresa = new DateTime(2021, 1, 2), Soltero = true },
-    new Persona { Nombre = "Nidia", Edad = 19, FechaIngresoALaEmpresa = new DateTime(2015, 11, 22), Soltero = true },
-    new Persona { Nombre = "Alejandro", Edad = 45, FechaIngresoALaEmpresa = new DateTime(2020, 4, 12), Soltero = false },
-    new Persona { Nombre = "Valentina", Edad = 24, FechaIngresoALaEmpresa = new DateTime(2021, 7, 8), Soltero = false },
-    new Persona { Nombre = "Roberto", Edad = 61, FechaIngresoALaEmpresa = DateTime.Now.AddDays(-1), Soltero = false },
-};
+string[] paises = { "Colombia","Ecuador","Mexico"};
 
-//Sintaxis metodo
-var personas25AñosOMenos = personas.Where(p => p.Edad <= 25).ToList();
-Console.WriteLine("Validando personas menores de 25 años: ");
-
-foreach (var persona in personas25AñosOMenos) 
+var paises1 = paises.Concat(paises);
+foreach (var paise in paises1) 
 {
-    Console.WriteLine($"{persona.Nombre} es menor de 25 años");
+    Console.WriteLine(paise);
 }
--- LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
+++ LINQ/obj/Debug/net8.0/LINQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860d3978f67b5085ecb4591bc576c5a070dbdf0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
